edf3c197a2685c1567ae30f14590f6f906dcc1c3
Karen <K.Blanchette07@gmail.com>
2017-08-14 14:23:34 -0500

    ORM structure

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,22 @@
+//import connection
+
+var connection = require('../config/connection.js');
+
 //CREATE METHODS
-//selectAll()
-//insertOne()
-//updateOne()
+var orm = {
+    //selectAll()
+    selectAll: function(){
+
+    },
+    //insertOne()
+    insertOne: function(){
+
+    },
+    //updateOne()
+    updateOne: function(){
+
+    }
+}
+
 //export ORM object 
-//module.exports
+module.exports = orm;
