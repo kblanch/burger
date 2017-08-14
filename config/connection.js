c2d58327d20811bbaca3012a4387833df9cee709
Karen <K.Blanchette07@gmail.com>
2017-08-14 14:19:20 -0500

    connection.js Node connected to MySQL

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,19 @@
 //Setup the code to connect Node to MySQL
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+});
+
+connection.connect(function(error){
+    if(error){
+        throw error;
+    }
+});
+
 //Export Connection
+module.exports = connection;
