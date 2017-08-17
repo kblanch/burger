f7b549ff020eefb16a8da02384671abfe3c47d2a
Karen <K.Blanchette07@gmail.com>
2017-08-17 15:17:56 -0500

    Controller Setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,5 +7,34 @@ var burger = require("../models/burger.js");
 //create router
 var router = express.Router();
 
+router.get("/", function (request, response){
+    burger.selectAll(function(data){
+        var allObjects = {
+            burgers: data
+        };
+        response.render("index", allObjects);
+    });
+});
 
+router.post("/", function(request, response){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ],
+    [
+        request.body.name, request.body.devoured
+    ], function() {
+        response.redirect("/");
+    });
+});
+
+router.put("/:id", function(request, response){
+    var condition = "id = " + request.params.id;
+
+    burger.updateOne({
+        devoured: request.body.devoured
+    }, condition, function(){
+        response.redirect("/");
+    });
+});
 //export router
+module.exports = router;
