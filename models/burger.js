d1d80c7e6a979f223063bb379b5815bd3e5b889e
Karen <K.Blanchette07@gmail.com>
2017-08-17 15:07:16 -0500

    Burger Model Complete

--- models/burger.js
+++ models/burger.js
@@ -3,9 +3,22 @@ var orm = require("../config/orm.js");
 
 //call orm functions
 var burger = {
-    
+    selectAll: function(callBack){
+        orm.selectAll("burgers", function(response){
+            callBack(response);
+        });
+    },
+    insertOne: function(columns, values, callBack){
+        orm.insertOne("burgers", columns, values, function(response){
+            callBack(response);
+        });
+    },
+    updateOne: function(values, condition, callBack){
+        orm.updateOne("burgers", values, condition, function(response){
+            callBack(response);
+        });
+    }
 }
-
 
 //export
 module.exports = burger;
